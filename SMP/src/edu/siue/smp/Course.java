aa1fd2f8c5203364368c85e860fb987df9c52bca
lupeter <lupeter@EB2029-08W.cs.siue.edu>
2015-09-01 15:36:44 -0500

    Edited Course class so that it now extends Event

--- SMP/src/edu/siue/smp/Course.java
+++ SMP/src/edu/siue/smp/Course.java
@@ -1,24 +1,61 @@
 package edu.siue.smp;
 
-public class Course {
-   private String crn;
-   private String dept;
-   private String crs;  
-   private String sec;  
-   
-   
-   public Course(){
-   this.crn = "";
-   this.dept = "";
-   this.crs = "";
-   this.sec = "";
-   }
-  public Course (String crn, String dept, String crs, String sec){
-       this.crn = crn;
-       this.dept = dept;
-       this.crs = crs;
-       this.sec = sec;
-   }
+import java.util.ArrayList;
+
+public class Course extends Event {
+
+    private String crn;
+    private String dept;
+    private String crs;
+    private String sec;
+    private ArrayList<String> days;
+    private int start;
+    private int end;
+    private String bldg;
+    private ArrayList<String> instructorPreference;
+    private ArrayList<TA> assignedTA;
+    private Coordinator coordinator;
+
+    public Course(int start, int end, Coordinator coordinator) {
+        super();
+        this.crn = "";
+        this.dept = "";
+        this.crs = "";
+        this.sec = "";
+
+        this.days = new ArrayList();
+        this.start = start;
+        this.end = end;
+        this.bldg = "";
+        this.instructorPreference = new ArrayList<String>();
+        this.assignedTA = new ArrayList<TA>();
+        this.coordinator = coordinator;
+    }
+
+    public void setCoordinator(Coordinator coordinator) {
+        this.coordinator = coordinator;
+    }
+
+    public Course(String crn, String dept, String crs, String sec, ArrayList<String> days,
+            int start,
+            int end,
+            String bldg,
+            ArrayList<String> instructorPreferencec,
+            ArrayList<TA> assignedTA,
+            Coordinator coordinator) {
+        this.crn = crn;
+        this.dept = dept;
+        this.crs = crs;
+        this.sec = sec;
+        this.days = days;
+        this.start = start;
+        this.end = end;
+        this.bldg = bldg;
+        this.instructorPreference = instructorPreference;
+        this.assignedTA = assignedTA;
+        this.coordinator = coordinator;
+    }
+
     public String getCrn() {
         return crn;
     }
@@ -50,8 +87,49 @@ public class Course {
     public void setSec(String sec) {
         this.sec = sec;
     }
- 
-   }
-   
-   
 
+    public ArrayList<String> getDays() {
+        return days;
+    }
+
+    public void setDays(ArrayList<String> days) {
+        this.days = days;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public ArrayList<String> getInstructorPreference() {
+        return instructorPreference;
+    }
+
+    public void setInstructorPreference(ArrayList<String> instructorPreference) {
+        this.instructorPreference = instructorPreference;
+    }
+
+    public ArrayList<TA> getAssignedTA() {
+        return assignedTA;
+    }
+
+    public void setAssignedTA(ArrayList<TA> assignedTA) {
+        this.assignedTA = assignedTA;
+    }
+
+    public Coordinator getCoordinator() {
+        return coordinator;
+    }
+
+}
