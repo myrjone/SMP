cec3195213e01744ec4db38f1b3c0265f32a0622
Scott Furlow <scottfurlow@gmail.com>
2015-09-01 14:57:25 -0500

    Created Coordinator object to store Coordinator info

---
+++ SMP/src/edu/siue/smp/Coordinator.java
@@ -0,0 +1,54 @@
+package edu.siue.smp;
+
+import java.util.UUID;
+
+public class Coordinator {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private UUID id;
+    
+    public Coordinator () {
+        this.firstName = "";
+        this.lastName = "";
+        this.email = "";
+        this.id = UUID.randomUUID();
+    }
+    
+    public Coordinator (String firstName,
+                            String lastName,
+                            String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.id = UUID.randomUUID();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
